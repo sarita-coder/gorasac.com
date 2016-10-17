e748e962ea46b4a119fa8f6b922513fe9020914a
sarita-coder <sarita.roman.colonio@gmail.com>
2016-10-17 17:19:01 -0500

    agregando particulas

--- js/main.js
+++ js/main.js
@@ -2,50 +2,108 @@ $('#menu-des').on('click',function(){
 	$('#main-nav').toggleClass('mostrar')
 	$('#menu-des').toggleClass('mover')
 })
+var logo=false;
+var menu=false;
+var scroll;
 
-$('.link-1').on('click',function(){
-	$('.hover-nav-text-0').toggleClass('visible')
-})
-$('.link-2').on('click',function(){
-	$('.hover-nav-text-1').toggleClass('visible')
-})
-$('.link-3').on('click',function(){
-	$('.hover-nav-text-2').toggleClass('visible')
-})
-$('.link-4').on('click',function(){
-	$('.hover-nav-text-3').toggleClass('visible')
-})
-$('.link-5').on('click',function(){
-	$('.hover-nav-text-4').toggleClass('visible')
-})
-$('.link-6').on('click',function(){
-	$('.hover-nav-text-5').toggleClass('visible')
-})
-/******/
 $(document).ready(function() {
+    $(window).scroll(function() {
+        scroll = $(window).scrollTop();
+        if(scroll> 200){
+            if(!logo){
+            $(".VOOXELL").css({"margin-top":"2rem","width":"8rem","height":"4rem"})
+            $(".logo-text").css({"display":"none"})
+            $(".logo-vooxel").css({"padding-top":"0rem","margin":"auto 90%"})
+                logo=true;
+            }
+        }
+        else {
+            if(logo){
+            $(".VOOXELL").css({"margin-top":"30%","width":"24rem","height":"10rem"})
+            $(".logo-text").css({"display":"block"})
+            $(".logo-vooxel").css({"padding-top":"10rem","margin":"auto"})
+               logo=false;
+            }
+        }
 
+    });
+    $(window).scroll(function() {
+        scroll = $(window).scrollTop();
+        if(scroll> 800){
+            if(!menu){
+            $(".hover-bajar").css({"color":"#C1C1C1"})
+            $(".hover-subir").css({"color":"#C1C1C1"})
+            $("#nav-parallax").css({"border-right":"0.01em solid rgba(216, 214, 214, 0.17)"})
+            $(".menu-bar").css({"border":"0.01em solid rgba(179, 170, 170, 0.91)"})
+            $(".bar").css({"background":"rgba(179, 170, 170, 0.91)"})
+                menu=true;
+            }
+        }
+
+        else {
+            if(menu){
+            $(".hover-bajar").css({"color":"white"})
+            $(".hover-subir").css({"color":"white"})
+            $(".hover-text").css({"color":"white"})
+            $("#nav-parallax").css({"border-right":"0.01em solid white"})
+            $(".menu-bar").css({"border":"0.01em solid white"})
+               menu=false;
+            }
+        }
+    });
+    /*** perfiles ***/
 	$('#Container').mixItUp();
-
     Materialize.updateTextFields();
-
+    /*** Formularios ***/
     $('#contac-frm1').click(function(){
     	$('#form1').fadeIn(1000);
     })
-
     $('#contac-frm2').click(function(){
     	$('#form2').fadeIn(1000);
     })
-
     $('.btn-cerrar').click(function(){
     	$('#form1').fadeOut(1000);
     })
-
     $('.btn-cerrar').click(function(){
     	$('#form2').fadeOut(1000);
     })
+    /** header **/
  });
 
 
+/***activar menu Header **/
+$(function(){
+        $('#menu-des').smoothScroll({
+          offset: -120,        
+          scrollTarget: $(this).val()
+       });
+       // Waypoints
+       $('.section').waypoint(  
+        function(direction) {
+            // al bajar
+        if (direction ==='down') {            
+             var wayID = $(this).attr('id'); 
+            if(wayID==('#slide-3')){
+                $(".nav .menu_item .hover-nav span").css({"color":"white"})
+            }           
+        } 
+        // al subir
+        else {
+            var previous = $(this).prev();
+            var wayID = $(previous).attr('id');                    
+            }
+            $('.active').removeClass('active');
+            $('.nav li a[href=#'+wayID+']').addClass('active');
+        }, { offset: '40%' }
 
-
-
+    );
+       /// StickNav  
+    var stickyNavTop = $('#menu-des').offset().top;  
+    var stickyNav = function(){  
+        var scrollTop = $(window).scrollTop();  
+    };       
+        stickyNav(); 
+    $(window).scroll(function() {
+        stickyNav();  
+    });  
+});
---
+++ js/particlesjs.json
@@ -0,0 +1,110 @@
+{
+  "particles": {
+    "number": {
+      "value": 109,
+      "density": {
+        "enable": false,
+        "value_area": 315.65905665290904
+      }
+    },
+    "color": {
+      "value": "#8ac832"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#2f5f35"
+      },
+      "polygon": {
+        "nb_sides": 7
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.30464829156444934,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 0.24362316369040352,
+        "opacity_min": 0.016241544246026904,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 4,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 82.8318756547372,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.08017060304327614,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6.413648243462092,
+      "direction": "top",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 1282.7296486924183,
+        "rotateY": 801.7060304327614
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": false,
+        "mode": "remove"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 119.88011988011988,
+        "line_linked": {
+          "opacity": 0.5347021708908134
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 0.40603860615067255,
+        "opacity": 0.17053621458328247,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 113.69080972218832,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+}
